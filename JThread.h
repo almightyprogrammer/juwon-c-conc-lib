825ab2d106d7e4f1214d9a6fc5db75a6c383cdf9
almightyprogrammer <lehman0308@gmail.com>
2025-11-16 14:14:56 +1300

    init

---
+++ JThread.h
@@ -0,0 +1,8 @@
+//
+// Created by humblefarmer on 16/11/2025.
+//
+
+#ifndef JUWON_CONC_LIB_JTHREAD_H
+#define JUWON_CONC_LIB_JTHREAD_H
+
+#endif //JUWON_CONC_LIB_JTHREAD_H
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef JUWON_CONC_LIB_LIBRARY_H
+#define JUWON_CONC_LIB_LIBRARY_H
+
+void hello(void);
+
+#endif // JUWON_CONC_LIB_LIBRARY_H
